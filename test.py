b77c6181224a5caa5a4023418853ea3f92b515ba
luke biggerstaff <luke.biggerstaff@gmail.com>
2017-11-29 10:02:20 -0600

    added Dockerfile and changed .travis.yml to run tests in docker

--- test.py
+++ test.py
@@ -8,3 +8,6 @@ class TestApp(unittest.TestCase):
     def test_app(self):
         result = app.hello()
         self.assertEqual(result, "hello world")
+
+if __name__ == '__main__':
+    unittest.main()
